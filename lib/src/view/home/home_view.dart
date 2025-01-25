47b3dd914b965fc5745787deb3e28c29baaef30f
Pedro Monteiro <83165668+pedroafmonteiro@users.noreply.github.com>
2025-01-25 14:13:17 +0000

    Implemented MVC pattern and a home view.

---
+++ lib/src/view/home/home_view.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  static const routeName = '/';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text('Home View'),
+      ),
+    );
+  }
+}
